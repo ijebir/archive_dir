14a4839aba7c2e9b0a521a7a3a27b6411d46811c
Idriss Jebir <idriss.jebir@gmail.com>
2025-08-26 19:34:30 +0100

    ds for files_dirs + dir scan

--- archive_dir.py
+++ archive_dir.py
@@ -1,6 +1,14 @@
+import os
 
-
+# { "path": "", "file": "", "type": "", "isIgnore": False }
 
 # We recursively to check the directory above root for files other folders
+def scan_directory(path):
+    for entry in os.listdir(path):
+        if os.path.isfile(os.path.join(path, entry)):
+            print(path, entry)
+
 # We need to provide the possibility of skipping a directory (all files will be included)
-# We need to zip the files at root with correct date and ts
+# What is the best way to store this info: (key, value) -> (directory_path, Boolean)
+
+# We need to zip the files at root with correct date and
