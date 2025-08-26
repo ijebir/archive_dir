30354f62fa26a2905923f46d3f78645b26f66e4c
Idriss Jebir <idriss.jebir@gmail.com>
2025-08-26 19:40:34 +0100

    more

--- archive_dir.py
+++ archive_dir.py
@@ -3,10 +3,16 @@ import os
 # { "path": "", "file": "", "type": "", "isIgnore": False }
 
 # We recursively to check the directory above root for files other folders
+# Maybe define a max_depth for recursively looking into dir
 def scan_directory(path):
     for entry in os.listdir(path):
         if os.path.isfile(os.path.join(path, entry)):
             print(path, entry)
+            # Add to DS
+        else:
+            # Check if directory shoulde be included, if so traverse
+            # otherwise skip it
+            print("")
 
 # We need to provide the possibility of skipping a directory (all files will be included)
 # What is the best way to store this info: (key, value) -> (directory_path, Boolean)
