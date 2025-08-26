8f88297f48544511d82d3512eb0ad835216d41db
Idriss Jebir <idriss.jebir@gmail.com>
2025-08-26 19:23:17 +0100

    general ideas

---
+++ archive_dir.py
@@ -0,0 +1,6 @@
+
+
+
+# We recursively to check the directory above root for files other folders
+# We need to provide the possibility of skipping a directory (all files will be included)
+# We need to zip the files at root with correct date and ts
